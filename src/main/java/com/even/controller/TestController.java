305688fb3234a1402968905a10e5570007ad80e6
Evenss <786007448@qq.com>
2017-12-01 10:46:25 +0800

    修改消息推送工具类

--- src/main/java/com/even/controller/TestController.java
+++ src/main/java/com/even/controller/TestController.java
@@ -1,15 +1,16 @@
 package com.even.controller;
 
+import com.jfinal.aop.Clear;
 import com.jfinal.core.Controller;
 
+@Clear
 public class TestController extends Controller {
+
     public void index(){
         renderText("test");
     }
 
     public void test(){
-        String a = getPara();
-        System.out.print("test data = " + a);
-        renderText(a);
+        renderText("123");
     }
 }
--- src/main/java/com/even/util/PushUtil.java
+++ src/main/java/com/even/util/PushUtil.java
@@ -1,69 +1,65 @@
 package com.even.util;
 
 import com.gexin.rp.sdk.base.IPushResult;
-import com.gexin.rp.sdk.base.impl.AppMessage;
-import com.gexin.rp.sdk.base.impl.ListMessage;
+import com.gexin.rp.sdk.base.impl.SingleMessage;
 import com.gexin.rp.sdk.base.impl.Target;
+import com.gexin.rp.sdk.exceptions.RequestException;
 import com.gexin.rp.sdk.http.IGtPush;
-import com.gexin.rp.sdk.template.NotificationTemplate;
-import com.gexin.rp.sdk.template.style.Style0;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.gexin.rp.sdk.template.LinkTemplate;
+import com.gexin.rp.sdk.template.style.*;
 
 public class PushUtil {
-    //定义常量, appId、appKey、masterSecret 采用本文档 "第二步 获取访问凭证 "中获得的应用配置
     private static String appId = "PSg0aZXd6O7m9pmR1kIfd9";
     private static String appKey = "v0mU1uSbKT8Jdce2nie0R2";
     private static String masterSecret = "lnvVt4cWb89DGueUF4kJU5";
     static String host = "http://sdk.open.api.igexin.com/apiex.htm";
 
-    static String CID1 = "fec997be9cbe11bd040d0139d07b6452";
-    static String CID2 = "";
-
     public static void main(String[] args) throws Exception {
-        // 配置返回每个用户返回用户状态，可选
-        System.setProperty("gexin_pushList_needDetails", "true");
-        // 配置返回每个别名及其对应cid的用户状态，可选
-         System.setProperty("gexin_pushList_needAliasDetails", "true");
-        IGtPush push = new IGtPush(host, appKey, masterSecret);
-        // 通知透传模板
-        NotificationTemplate template = notificationTemplateDemo();
-        ListMessage message = new ListMessage();
-        message.setData(template);
-        // 设置消息离线，并设置离线时间
-        message.setOffline(true);
-        // 离线有效时间，单位为毫秒，可选
-        message.setOfflineExpireTime(24 * 1000 * 3600);
-        // 配置推送目标
-        List targets = new ArrayList();
-        Target target1 = new Target();
-        Target target2 = new Target();
-        target1.setAppId(appId);
-//        target1.setClientId(CID1);
-             target1.setAlias("112");
-        target2.setAppId(appId);
-        target2.setClientId(CID2);
-        //     target2.setAlias(Alias2);
-        targets.add(target1);
-        targets.add(target2);
-        // taskId用于在推送时去查找对应的message
-        String taskId = push.getContentId(message);
-        IPushResult ret = push.pushMessageToList(taskId, targets);
-        System.out.println(ret.getResponse().toString());
+        pushInfo("15957159506", "标题", "内容");
     }
 
-    public static NotificationTemplate notificationTemplateDemo() {
-        NotificationTemplate template = new NotificationTemplate();
+    public static void pushInfo(String phone, String title, String content) {
+        pushInfo(phone, title, content, "http://www.12306.cn/mormhweb/");
+    }
+
+    public static void pushInfo(String phone, String title, String content, String clickUrl) {
+        IGtPush push = new IGtPush(host, appKey, masterSecret);
+        LinkTemplate template = linkTemplateDemo(title, content, clickUrl);
+        SingleMessage message = new SingleMessage();
+        message.setOffline(true);
+        // 离线有效时间
+        message.setOfflineExpireTime(3600 * 1000);//1h
+        message.setData(template);
+
+        Target target = new Target();
+        target.setAppId(appId);
+//        target.setClientId(CID);
+        target.setAlias(phone);
+        IPushResult ret;
+        try {
+            ret = push.pushMessageToSingle(message, target);
+        } catch (RequestException e) {
+            e.printStackTrace();
+            ret = push.pushMessageToSingle(message, target, e.getRequestId());
+        }
+        if (ret != null) {
+            System.out.println(ret.getResponse().toString());
+        } else {
+            System.out.println("服务器响应异常");
+        }
+    }
+
+    private static LinkTemplate linkTemplateDemo(String title, String content, String clickUrl) {
+        LinkTemplate template = new LinkTemplate();
         // 设置APPID与APPKEY
         template.setAppId(appId);
         template.setAppkey(appKey);
 
-        Style0 style = new Style0();
+        Style1 style = new Style1();
         // 设置通知栏标题与内容
-        style.setTitle("请输入通知栏标题");
-        style.setText("请输入通知栏内容");
-        // 配置通知栏图标
+        style.setTitle(title);
+        style.setText(content);
+        //todo 配置通知栏图标 客户端修改
         style.setLogo("icon.png");
         // 配置通知栏网络图标
         style.setLogoUrl("");
@@ -73,9 +69,8 @@ public class PushUtil {
         style.setClearable(true);
         template.setStyle(style);
 
-        // 透传消息设置，1为强制启动应用，客户端接收到消息后就会立即启动应用；2为等待应用启动
-        template.setTransmissionType(2);
-        template.setTransmissionContent("请输入您要透传的内容");
+        // 设置打开的网址地址
+        template.setUrl(clickUrl);
         return template;
     }
 }
