24ca8afc0ec89259b4a1b7bdffc7a8b28b79697c
Even <liyihang@tpson.cn>
2018-01-06 16:50:13 +0800

    test port

--- src/main/java/com/even/controller/TestController.java
+++ src/main/java/com/even/controller/TestController.java
@@ -11,12 +11,10 @@ public class TestController extends Controller {
     private static Logger log = Logger.getLogger(TestController.class);
 
     public void index(){
-        renderText("test2");
+        renderText("test1");
     }
 
     public void test(){
         renderText("123");
-        PLog.i("PLog test logs!");
-        PLog.e("PLog test logs!");
     }
 }
